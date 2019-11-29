bbe4a0542311fb2e4d19e69e9da0ea750e8cf48e
Gregor Lennie <gregorlennie@hotmail.co.uk>
2019-11-29 11:32:19 +0000

    Adds transactions schema, tested with psql

--- db/spending_tracker.sql
+++ db/spending_tracker.sql
@@ -1,12 +1,20 @@
+DROP TABLE IF EXISTS transactions;
 DROP TABLE IF EXISTS merchants;
 DROP TABLE IF EXISTS tags;
 
 CREATE TABLE merchants (
   id SERIAL PRIMARY KEY,
-  merchant_name VARCHAR
+  merchant_name VARCHAR NOT NULL
 );
 
 CREATE TABLE tags (
   id SERIAL PRIMARY KEY,
-  tag_name VARCHAR
+  tag_name VARCHAR NOT NULL
+);
+
+CREATE TABLE transactions (
+  id SERIAL PRIMARY KEY,
+  merchant_id INT REFERENCES merchants(id),
+  tag_id INT REFERENCES tags(id),
+  amount DECIMAL(4,2)
 )
