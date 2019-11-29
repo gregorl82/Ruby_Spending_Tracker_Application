f53bb5a1ba204873c60a64bad4dd8a62bcd16971
Gregor Lennie <gregorlennie@hotmail.co.uk>
2019-11-29 21:04:49 +0000

    Adds index route and view for tags, tested in browser

---
+++ controllers/tag_controller.rb
@@ -0,0 +1,13 @@
+require('sinatra')
+require('sinatra/contrib/all')
+require('pry')
+require_relative('../models/transaction.rb')
+require_relative('../models/tag.rb')
+require_relative('../models/merchant.rb')
+also_reload('../models/*')
+
+get '/tags' do
+  tags = Tag.all()
+  @tags = tags.sort_by! {|tag| tag.id}
+  erb(:"tags/index")
+end
