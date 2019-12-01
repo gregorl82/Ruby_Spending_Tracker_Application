338b6e1650514fddc3bfaa8e181d25627def1d38
Gregor Lennie <gregorlennie@hotmail.co.uk>
2019-12-01 15:57:38 +0000

    Adds create budgets route and view, tested in browser

--- controllers/budget_controller.rb
+++ controllers/budget_controller.rb
@@ -4,10 +4,21 @@ require('pry')
 require_relative('../models/transaction.rb')
 require_relative('../models/tag.rb')
 require_relative('../models/merchant.rb')
+require_relative('../models/budget.rb')
 also_reload('../models/*')
 
 get '/budgets' do
   budgets = Budget.all()
   @budgets = budgets.sort_by {|budget| budget.id}
   erb(:"budgets/index")
+end
+
+get '/budgets/new' do
+  erb(:"budgets/new")
+end
+
+post '/budgets' do
+  budget = Budget.new(params)
+  budget.save()
+  redirect to ("/budgets")
 end
