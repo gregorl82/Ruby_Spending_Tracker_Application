e663c9c6f51338dd59e233e74847d07892d2d7ca
Gregor Lennie <gregorlennie@hotmail.co.uk>
2019-12-01 11:08:06 +0000

    Adds all and find_by_id methods to Budget class, tested in pry

--- models/budget.rb
+++ models/budget.rb
@@ -18,6 +18,19 @@ class Budget
     @id = result[0]['id'].to_i()
   end
 
+  def self.all()
+    sql = "SELECT * FROM budgets"
+    budgets = SqlRunner.run(sql)
+    return budgets.map {|budget| Budget.new(budget)}
+  end
+
+  def self.find_by_id(id)
+    sql = "SELECT * FROM budgets WHERE id = $1"
+    values = [id]
+    result = SqlRunner.run(sql, values).first()
+    return Budget.new(result)
+  end
+
   def delete()
     sql = "DELETE FROM budgets WHERE id = $1"
     values = [@id]
