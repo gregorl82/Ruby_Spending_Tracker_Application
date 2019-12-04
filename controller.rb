10ef0cdb4153698d9d89d7dab4d3baec1304547b
gregorl82 <gregorlennie@hotmail.co.uk>
2019-12-04 10:51:06 +0000

    Updates controller names

--- controller.rb
+++ controller.rb
@@ -1,9 +1,9 @@
 require('sinatra')
 require('sinatra/contrib/all')
-require_relative('controllers/transaction_controller.rb')
-require_relative('controllers/tag_controller.rb')
-require_relative('controllers/merchant_controller.rb')
-require_relative('controllers/budget_controller.rb')
+require_relative('controllers/transactions_controller.rb')
+require_relative('controllers/tags_controller.rb')
+require_relative('controllers/merchants_controller.rb')
+require_relative('controllers/budgets_controller.rb')
 
 get '/' do
   erb(:index)
